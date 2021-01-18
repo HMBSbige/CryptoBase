aff7e9e6905e836827b5744016bb9911c93417d0
Bruce Wayne <admin@bige0.com>
2021-01-18 12:53:47 +0800

    Refine

--- CryptoBase/Digests/MD5/BcMD5Digest.cs
+++ CryptoBase/Digests/MD5/BcMD5Digest.cs
@@ -1,7 +1,6 @@
 using CryptoBase.Abstractions.Digests;
 using Org.BouncyCastle.Crypto.Digests;
 using System;
-using System.Buffers;
 using System.Threading;
 
 namespace CryptoBase.Digests.MD5
@@ -12,23 +11,7 @@ namespace CryptoBase.Digests.MD5
 
 		public override void ComputeHash(in ReadOnlySpan<byte> origin, Span<byte> destination)
 		{
-			var buffer = ArrayPool<byte>.Shared.Rent(origin.Length);
-			var outBuffer = ArrayPool<byte>.Shared.Rent(Length);
-			try
-			{
-				origin.CopyTo(buffer);
-
-				Hasher.Value!.Reset();
-				Hasher.Value!.BlockUpdate(buffer, 0, origin.Length);
-				Hasher.Value!.DoFinal(outBuffer, 0);
-
-				outBuffer.AsSpan(0, Length).CopyTo(destination);
-			}
-			finally
-			{
-				ArrayPool<byte>.Shared.Return(buffer);
-				ArrayPool<byte>.Shared.Return(outBuffer);
-			}
+			Utils.BcComputeHash(Hasher.Value!, Length, origin, destination);
 		}
 	}
 }
--- CryptoBase/Utils.cs
+++ CryptoBase/Utils.cs
@@ -1,4 +1,6 @@
+using Org.BouncyCastle.Crypto;
 using System;
+using System.Buffers;
 using System.Security.Cryptography;
 
 namespace CryptoBase
@@ -16,6 +18,27 @@ namespace CryptoBase
 			Span<byte> bytes = new byte[size];
 			RandBytes(bytes);
 			return bytes;
+		}
+
+		internal static void BcComputeHash(IDigest hash, int length, in ReadOnlySpan<byte> origin, Span<byte> destination)
+		{
+			var buffer = ArrayPool<byte>.Shared.Rent(origin.Length);
+			var outBuffer = ArrayPool<byte>.Shared.Rent(length);
+			try
+			{
+				origin.CopyTo(buffer);
+
+				hash.Reset();
+				hash.BlockUpdate(buffer, 0, origin.Length);
+				hash.DoFinal(outBuffer, 0);
+
+				outBuffer.AsSpan(0, length).CopyTo(destination);
+			}
+			finally
+			{
+				ArrayPool<byte>.Shared.Return(buffer);
+				ArrayPool<byte>.Shared.Return(outBuffer);
+			}
 		}
 	}
 }
