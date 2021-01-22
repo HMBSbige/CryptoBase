a233766ddf44916d9827b848b907ed9658760b12
Bruce Wayne <admin@bige0.com>
2021-01-22 13:10:59 +0800

    Refine

--- CryptoBase/IntrinsicsUtils.cs
+++ CryptoBase/IntrinsicsUtils.cs
@@ -11,7 +11,9 @@ namespace CryptoBase
 		private readonly static Vector128<byte> Rot8_128 = Vector128.Create((byte)3, 0, 1, 2, 7, 4, 5, 6, 11, 8, 9, 10, 15, 12, 13, 14);
 		private readonly static Vector128<byte> Rot16_128 = Vector128.Create((byte)2, 3, 0, 1, 6, 7, 4, 5, 10, 11, 8, 9, 14, 15, 12, 13);
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static readonly Vector256<uint> Permute0 = Vector256.Create(5, 6, 7, 4, 3, 0, 1, 2).AsUInt32();
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 		public static uint AndNot(uint left, uint right)
 		{
 			if (Bmi1.IsSupported)
@@ -21,37 +23,37 @@ namespace CryptoBase
 			return ~left & right;
 		}
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 		public static Vector256<uint> RotateLeft(this Vector256<uint> value, byte offset)
 		{
 			return Avx2.Or(Avx2.ShiftLeftLogical(value, offset), Avx2.ShiftRightLogical(value, (byte)(32 - offset)));
 		}
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 		public static Vector256<uint> RotateLeft8(this Vector256<uint> value)
 		{
 			return Avx2.Shuffle(value.AsByte(), Rot8).AsUInt32();
 		}
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 		public static Vector256<uint> RotateLeft16(this Vector256<uint> value)
 		{
 			return Avx2.Shuffle(value.AsByte(), Rot16).AsUInt32();
 		}
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 		public static Vector128<uint> RotateLeft(this Vector128<uint> value, byte offset)
 		{
 			return Sse2.Or(Sse2.ShiftLeftLogical(value, offset), Sse2.ShiftRightLogical(value, (byte)(32 - offset)));
 		}
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 		public static Vector128<uint> RotateLeft8(this Vector128<uint> value)
 		{
 			return Ssse3.Shuffle(value.AsByte(), Rot8_128).AsUInt32();
 		}
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 		public static Vector128<uint> RotateLeft16(this Vector128<uint> value)
 		{
 			return Ssse3.Shuffle(value.AsByte(), Rot16_128).AsUInt32();
@@ -59,25 +61,11 @@ namespace CryptoBase
 
 		/// <summary>
 		/// a+=b
-		/// c+=d
 		/// </summary>
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static void Add(ref Vector128<uint> a, Vector128<uint> b, ref Vector128<uint> c, Vector128<uint> d)
+		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+		public static void Add(this ref Vector128<uint> a, Vector128<uint> b)
 		{
-			if (Avx2.IsSupported)
-			{
-				var v0 = Vector256.Create(a, c);
-				var v1 = Vector256.Create(b, d);
-
-				var v = Avx2.Add(v0, v1);
-
-				a = v.GetLower();
-				c = v.GetUpper();
-				return;
-			}
-
 			a = Sse2.Add(a, b);
-			c = Sse2.Add(c, d);
 		}
 
 		/// <summary>
@@ -148,46 +136,76 @@ namespace CryptoBase
 			{
 				SalsaQuarterRound(ref x0, ref x1, ref x2, ref x3);
 
-				Utils.Swap(ref x0, ref x2);
-				x0 = Sse2.Shuffle(x0, 0b00_11_10_01); // 12 1 6 11 => 1 6 11 12
-				x2 = Sse2.Shuffle(x2, 0b10_01_00_11); // 4 9 14 3 => 3 4 9 14
+				SalsaShuffle(ref x0, ref x2);
 				x3 = Sse2.Shuffle(x3, 0b01_00_11_10); // 8 13 2 7 => 2 7 8 13
 
 				SalsaQuarterRound(ref x0, ref x1, ref x2, ref x3);
 
-				Utils.Swap(ref x0, ref x2);
-				x0 = Sse2.Shuffle(x0, 0b00_11_10_01); // 3 4 9 14 => 4 9 14 3 
-				x2 = Sse2.Shuffle(x2, 0b10_01_00_11); // 1 6 11 12 => 12 1 6 11
+				SalsaShuffle(ref x0, ref x2);
 				x3 = Sse2.Shuffle(x3, 0b01_00_11_10); // 2 7 8 13 => 8 13 2 7
 			}
 
-			var v0 = Sse2.Shuffle(x0, 0b10_01_00_11); // 4 9 14 3 => 3 4 9 14
-			var v1 = x1; // 0 5 10 15
-			var v2 = Sse2.Shuffle(x2, 0b00_11_10_01); // 12 1 6 11 => 1 6 11 12
-			var v3 = Sse2.Shuffle(x3, 0b01_00_11_10); // 8 13 2 7 => 2 7 8 13
+			SalsaShuffle(ref x0, ref x1, ref x2, ref x3);
 
-			var t0 = Sse2.UnpackLow(v0, v1); // 3 0 4 5
-			var t1 = Sse2.UnpackLow(v2, v3); // 1 2 6 7
-			var t2 = Sse2.UnpackHigh(v0, v1); // 9 10 14 15
-			var t3 = Sse2.UnpackHigh(v2, v3); // 11 8 12 13
+			x0.Add(s0);
+			x1.Add(s1);
+			x2.Add(s2);
+			x3.Add(s3);
 
-			v0 = Sse2.Shuffle(Sse2.UnpackLow(t0, t1), 0b00_11_01_10); // 3 1 0 2 => 0 1 2 3
-			v1 = Sse2.Shuffle(Sse2.UnpackHigh(t0, t1), 0b11_01_10_00); // 4 6 5 7 => 4 5 6 7
-			v2 = Sse2.Shuffle(Sse2.UnpackLow(t2, t3), 0b01_10_00_11); // 9 11 10 8 => 8 9 10 11
-			v3 = Sse2.Shuffle(Sse2.UnpackHigh(t2, t3), 0b10_00_11_01); // 14 12 15 13 => 12 13 14 15
-
-			Add(ref v0, s0, ref v1, s1);
-			Add(ref v2, s2, ref v3, s3);
-
-			Sse2.Store(stream, v0.AsByte());
-			Sse2.Store(stream + 16, v1.AsByte());
-			Sse2.Store(stream + 32, v2.AsByte());
-			Sse2.Store(stream + 48, v3.AsByte());
+			Sse2.Store(stream, x0.AsByte());
+			Sse2.Store(stream + 16, x1.AsByte());
+			Sse2.Store(stream + 32, x2.AsByte());
+			Sse2.Store(stream + 48, x3.AsByte());
 
 			if (++*(state + 8) == 0)
 			{
 				++*(state + 9);
 			}
+		}
+
+		/// <summary>
+		/// 0 1 2 3
+		/// 4 5 6 7
+		/// =>
+		/// 5 6 7 4
+		/// 3 0 1 2
+		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+		public static void SalsaShuffle(ref Vector128<uint> a, ref Vector128<uint> b)
+		{
+			Utils.Swap(ref a, ref b);
+			a = Sse2.Shuffle(a, 0b00_11_10_01); // 12 1 6 11 => 1 6 11 12
+			b = Sse2.Shuffle(b, 0b10_01_00_11); // 4 9 14 3 => 3 4 9 14
+		}
+
+		/// <summary>
+		/// 4 9 14 3
+		/// 0 5 10 15
+		/// 12 1 6 11
+		/// 8 13 2 7
+		/// =>
+		/// 0 1 2 3
+		/// 4 5 6 7
+		/// 8 9 10 11
+		/// 12 13 14 15
+		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+		public static void SalsaShuffle(ref Vector128<uint> a, ref Vector128<uint> b, ref Vector128<uint> c, ref Vector128<uint> d)
+		{
+			a = Sse2.Shuffle(a, 0b10_01_00_11); // 4 9 14 3 => 3 4 9 14
+												// 0 5 10 15
+			c = Sse2.Shuffle(c, 0b00_11_10_01); // 12 1 6 11 => 1 6 11 12
+			d = Sse2.Shuffle(d, 0b01_00_11_10); // 8 13 2 7 => 2 7 8 13
+
+			var t0 = Sse2.UnpackLow(a, b); // 3 0 4 5
+			var t1 = Sse2.UnpackLow(c, d); // 1 2 6 7
+			var t2 = Sse2.UnpackHigh(a, b); // 9 10 14 15
+			var t3 = Sse2.UnpackHigh(c, d); // 11 8 12 13
+
+			a = Sse2.Shuffle(Sse2.UnpackLow(t0, t1), 0b00_11_01_10); // 3 1 0 2 => 0 1 2 3
+			b = Sse2.Shuffle(Sse2.UnpackHigh(t0, t1), 0b11_01_10_00); // 4 6 5 7 => 4 5 6 7
+			c = Sse2.Shuffle(Sse2.UnpackLow(t2, t3), 0b01_10_00_11); // 9 11 10 8 => 8 9 10 11
+			d = Sse2.Shuffle(Sse2.UnpackHigh(t2, t3), 0b10_00_11_01); // 14 12 15 13 => 12 13 14 15
 		}
 	}
 }
