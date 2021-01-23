381fb3852e4b7a1b4c37b588668187ac423f6771
Bruce Wayne <admin@bige0.com>
2021-01-23 19:31:00 +0800

    Add more test

--- UnitTest/SM3Test.cs
+++ UnitTest/SM3Test.cs
@@ -28,6 +28,7 @@ namespace UnitTest
 		[DataRow(@"", @"1ab21d8355cfa17f8e61194831e81a8f22bec8c728fefb747ed035eb5082aa2b")]
 		[DataRow(@"abc", @"66c7f0f462eeedd9d1f2d46bdc10e4e24167c4875cf2f7a2297da02b8f4ba8e0")]
 		[DataRow(@"abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd", @"debe9ff92275b8a138604889c18e5a4d6fdb70e5387e5765293dcba39c0c5732")]
+		[DataRow(@"1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890", @"f07b76f62b616de94e6ef9733db1953b8e3fa55014c3f4667abf2ea60aa44250")]
 		public void SM3DigestTest(string str, string sm3Str)
 		{
 			SM3DigestTest(new BcSM3Digest(), str, sm3Str);
