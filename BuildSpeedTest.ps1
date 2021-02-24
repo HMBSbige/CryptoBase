6ec4fe3be89e3cc44300a4dac2f436bfaaf15b98
Bruce Wayne <admin@bige0.com>
2021-02-24 16:56:20 +0800

    Add simple speed test

---
+++ BuildSpeedTest.ps1
@@ -0,0 +1,25 @@
+param([string]$rid = 'linux-x64')
+$ErrorActionPreference = 'Stop'
+
+Write-Host 'dotnet SDK version'
+dotnet --version
+
+$net_tfm = 'net5.0'
+$configuration = 'Release'
+$output_dir = "$PSScriptRoot\CryptoBase.SpeedTest\bin\$configuration"
+$proj_path = "$PSScriptRoot\CryptoBase.SpeedTest\CryptoBase.SpeedTest.csproj"
+function New-App
+{
+    param([string]$rid)
+    Write-Host 'Building'
+
+    $outdir = "$output_dir\$net_tfm"
+    $publishDir = "$outdir\publish"
+
+    Remove-Item $publishDir -Recurse -Force -Confirm:$false -ErrorAction Ignore
+
+    dotnet publish "$proj_path" -c $configuration -f $net_tfm -p:PublishSingleFile=true --self-contained true -p:PublishTrimmed=True -p:TrimMode=Link -r $rid
+    if ($LASTEXITCODE) { exit $LASTEXITCODE }
+}
+
+New-App $rid
---
+++ CryptoBase.SpeedTest/Program.cs
@@ -0,0 +1,51 @@
+using CryptoBase;
+using CryptoBase.SpeedTest;
+using System;
+using System.Diagnostics;
+
+Console.WriteLine("Start CryptoBase.SpeedTest...");
+#if DEBUG
+Console.WriteLine("On Debug mode");
+#endif
+if (Debugger.IsAttached)
+{
+	Console.WriteLine("Debugger attached!");
+}
+
+Console.WriteLine($@"OS Version: {Environment.OSVersion}");
+Console.WriteLine($@".NET Version: {Environment.Version}");
+
+var key32 = CryptoTest.Key.Slice(0, 32).ToArray();
+var key16 = CryptoTest.Key.Slice(0, 16).ToArray();
+var iv16 = CryptoTest.IV.Slice(0, 16).ToArray();
+var iv24 = CryptoTest.IV.Slice(0, 24).ToArray();
+
+Console.Write(@"Testing AesCtr: ");
+CryptoTest.Test(StreamCryptoCreate.AesCtr(key16, iv16));
+Console.Write(@"Testing Sm4Ctr: ");
+CryptoTest.Test(StreamCryptoCreate.Sm4Ctr(key16, iv16));
+Console.Write(@"Testing AesCfb: ");
+CryptoTest.Test(StreamCryptoCreate.AesCfb(true, key16, iv16));
+Console.Write(@"Testing Sm4Cfb: ");
+CryptoTest.Test(StreamCryptoCreate.Sm4Cfb(true, key16, iv16));
+Console.Write(@"Testing RC4: ");
+CryptoTest.Test(StreamCryptoCreate.Rc4(key16));
+Console.Write(@"Testing ChaCha20: ");
+CryptoTest.Test(StreamCryptoCreate.ChaCha20(key32, iv16));
+Console.Write(@"Testing ChaCha20Original: ");
+CryptoTest.Test(StreamCryptoCreate.ChaCha20Original(key32, iv16));
+Console.Write(@"Testing XChaCha20: ");
+CryptoTest.Test(StreamCryptoCreate.XChaCha20(key32, iv24));
+Console.Write(@"Testing Salsa20: ");
+CryptoTest.Test(StreamCryptoCreate.Salsa20(key32, iv16));
+Console.Write(@"Testing XSalsa20: ");
+CryptoTest.Test(StreamCryptoCreate.XSalsa20(key32, iv24));
+
+Console.Write(@"Testing AES-GCM: ");
+CryptoTest.Test(AEADCryptoCreate.AesGcm(key16));
+Console.Write(@"Testing SM4-GCM: ");
+CryptoTest.Test(AEADCryptoCreate.Sm4Gcm(key16));
+Console.Write(@"Testing ChaCha20Poly1305: ");
+CryptoTest.Test(AEADCryptoCreate.ChaCha20Poly1305(key32));
+Console.Write(@"Testing XChaCha20Poly1305: ");
+CryptoTest.Test(AEADCryptoCreate.XChaCha20Poly1305(key32), 24);
