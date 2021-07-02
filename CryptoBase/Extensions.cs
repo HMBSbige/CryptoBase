dbede3875d54cd6609da16cd7d9fe86af86a6a62
Bruce Wayne <admin@bige0.com>
2021-07-02 17:41:13 +0800

    Add hex extension test

--- CryptoBase/Extensions.cs
+++ CryptoBase/Extensions.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Buffers.Binary;
-using System.Linq;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 
@@ -8,8 +7,6 @@ namespace CryptoBase
 {
 	public static class Extensions
 	{
-		private const string Alphabet = @"0123456789abcdef";
-
 		#region SodiumIncrement
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -102,39 +99,6 @@ namespace CryptoBase
 				{
 				}
 			}
-		}
-
-		public static string ToHex(this in Span<byte> bytes)
-		{
-			var length = bytes.Length << 1;
-			Span<char> c = length switch
-			{
-				< 3 * 1024 / sizeof(char) => stackalloc char[length],
-				_ => GC.AllocateUninitializedArray<char>(length)
-			};
-
-			var i = 0;
-			var j = 0;
-
-			while (i < bytes.Length)
-			{
-				var b = bytes[i++];
-				c[j++] = Alphabet[b >> 4];
-				c[j++] = Alphabet[b & 0xF];
-			}
-
-			var result = new string(c);
-
-			return result;
-		}
-
-		public static byte[] FromHex(this string hex)
-		{
-			hex = hex.Replace(@"0x", string.Empty).Replace(@"-", string.Empty);
-			return Enumerable.Range(0, hex.Length)
-					.Where(x => (x & 1) == 0)
-					.Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
-					.ToArray();
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
---
+++ UnitTest/HexExtensionTest.cs
@@ -0,0 +1,30 @@
+using CryptoBase;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text;
+
+namespace UnitTest
+{
+	[TestClass]
+	public class HexExtensionTest
+	{
+		[TestMethod]
+		[DataRow(@"0123456789ABCDEFFEDCBA9876543210", @"3031323334353637383941424344454646454443424139383736353433323130")]
+		[DataRow(@"~中文测试12！", @"7ee4b8ade69687e6b58be8af953132efbc81")]
+		public void ToHexTest(string input, string expected)
+		{
+			Span<byte> span = Encoding.UTF8.GetBytes(input);
+			Assert.AreEqual(expected, span.ToHex());
+		}
+
+		[TestMethod]
+		[DataRow(@"30-31-32-33-34-35-36-37-38-39-41-42-43-44-45-46-46-45-44-43-42-41-39-38-37-36-35-34-33-32-31-30", @"0123456789ABCDEFFEDCBA9876543210")]
+		[DataRow(@"0x7e0xe40xb80xad0xe60x960x870xe60xb50x8b0xe80xaf0x950x310x320xef0xbc0x81", @"~中文测试12！")]
+		[DataRow(@"31313435313431393139383130", @"1145141919810")]
+		public void FromHexTest(string input, string expected)
+		{
+			Span<byte> span = Encoding.UTF8.GetBytes(expected);
+			Assert.IsTrue(span.SequenceEqual(input.FromHex()));
+		}
+	}
+}
