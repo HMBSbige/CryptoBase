e7f411998b0b0ddea2c6af1b7849b03ba0872119
Bruce Wayne <admin@bige0.com>
2021-07-05 16:19:46 +0800

    Revert "Oops"
    This reverts commit e5e96454344566f21fdd1b9125b338d3980c6be6.

--- CryptoBase/HexExtensions.cs
+++ CryptoBase/HexExtensions.cs
@@ -1,27 +1,64 @@
 using System;
-using System.Runtime.CompilerServices;
 
 namespace CryptoBase
 {
 	public static class HexExtensions
 	{
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static string ToHex(this Span<byte> bytes)
+		private const string Alphabet = @"0123456789abcdef";
+
+		public static string ToHex(this in Span<byte> bytes)
 		{
-			return Convert.ToHexString(bytes);
+			return ToHex((ReadOnlySpan<byte>)bytes);
 		}
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static string ToHex(this ReadOnlySpan<byte> bytes)
+		public static string ToHex(this in ReadOnlySpan<byte> bytes)
 		{
-			return Convert.ToHexString(bytes);
+			var length = bytes.Length << 1;
+			var c = length switch
+			{
+				< 3 * 1024 / sizeof(char) => stackalloc char[length],
+				_ => GC.AllocateUninitializedArray<char>(length)
+			};
+
+			var i = 0;
+			var j = 0;
+
+			while (i < bytes.Length)
+			{
+				var b = bytes[i++];
+				c[j++] = Alphabet[b >> 4];
+				c[j++] = Alphabet[b & 0xF];
+			}
+
+			var result = new string(c);
+
+			return result;
 		}
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static byte[] FromHex(this string hex)
 		{
 			hex = hex.Replace(@"0x", string.Empty).Replace(@"-", string.Empty);
-			return Convert.FromHexString(hex);
+
+			if ((hex.Length & 1) is not 0)
+			{
+				throw new ArgumentException($@"{nameof(hex)} length must be even");
+			}
+
+			var length = hex.Length >> 1;
+			var buffer = GC.AllocateUninitializedArray<byte>(length);
+
+			for (int i = 0, j = 0; i < length; ++i, ++j)
+			{
+				// Convert first half of byte
+				var c = hex[j];
+				buffer[i] = (byte)((c > '9' ? (c > 'Z' ? (c - 'a' + 10) : (c - 'A' + 10)) : (c - '0')) << 4);
+
+				// Convert second half of byte
+				c = hex[++j];
+				buffer[i] |= (byte)(c > '9' ? (c > 'Z' ? (c - 'a' + 10) : (c - 'A' + 10)) : (c - '0'));
+			}
+
+			return buffer;
 		}
 	}
 }
