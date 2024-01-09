60d31b215906bda99cf4054a14a53589b584ccf1
Bruce Wayne <admin@bige0.com>
2024-01-09 21:34:51 +0800

    refactor: drop ring

--- native/cryptobase/src/lib.rs
+++ native/cryptobase/src/lib.rs
@@ -2,12 +2,6 @@
 mod digest_ffi;
 #[cfg(feature = "md5")]
 pub mod md5;
-#[cfg(any(feature = "sha1_ring", feature = "sha256_ring"))]
-mod digest_ring_ffi;
-#[cfg(feature = "sha1_ring")]
-pub mod sha1_ring;
-#[cfg(feature = "sha256_ring")]
-pub mod sha256_ring;
 #[cfg(feature = "sha1")]
 pub mod sha1;
 #[cfg(feature = "sha224")]
