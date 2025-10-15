d03b3692f81cd3a954e88450d16a6723da213967
Bruce Wayne <admin@bige0.com>
2025-10-15 14:05:06 +0800

    fix: #88

--- test/CryptoBase.Tests/GHashTest.cs
+++ test/CryptoBase.Tests/GHashTest.cs
@@ -6,6 +6,18 @@ namespace CryptoBase.Tests;
 
 public class GHashTest
 {
+	/// <summary>
+	/// https://csrc.nist.rip/groups/ST/toolkit/BCM/documents/proposedmodes/gcm/gcm-spec.pdf
+	/// https://www.intel.cn/content/dam/www/public/us/en/documents/white-papers/carry-less-multiplication-instruction-in-gcm-mode-paper.pdf
+	/// </summary>
+	public static readonly TheoryData<string, string, string> Data = new()
+	{
+		{ @"dfa6bf4ded81db03ffcaff95f830f061", @"952b2a56a5604ac0b32b6656a05b40b6", @"da53eb0ad2c55bb64fc4802cc3feda60" },
+		{ @"66e94bd4ef8a2c3b884cfa59ca342b2e", @"0388dace60b6a392f328c2b971b2fe78", @"5e2ec746917062882c85b0685353deb7" },
+		{ @"66e94bd4ef8a2c3b884cfa59ca342b2e", @"0388dace60b6a392f328c2b971b2fe7800000000000000000000000000000080", @"f38cbb1ad69223dcc3457ae5b6b0f885" },
+		{ @"66e94bd4ef8a2c3b884cfa59ca342b2e", @"0388dace60b6a392f328c2b971b2fe7ad2c55bb64f", @"c1d3b69b62c9a392687aaf55d95a1df6" }
+	};
+
 	private static void Test_Internal(IMac mac, string plainHex, string cipherHex)
 	{
 		Span<byte> plain = plainHex.FromHex();
@@ -36,20 +48,20 @@ public class GHashTest
 		mac.Dispose();
 	}
 
-	/// <summary>
-	/// https://csrc.nist.rip/groups/ST/toolkit/BCM/documents/proposedmodes/gcm/gcm-spec.pdf
-	/// https://www.intel.cn/content/dam/www/public/us/en/documents/white-papers/carry-less-multiplication-instruction-in-gcm-mode-paper.pdf
-	/// </summary>
 	[Theory]
-	[InlineData(@"dfa6bf4ded81db03ffcaff95f830f061", @"952b2a56a5604ac0b32b6656a05b40b6", @"da53eb0ad2c55bb64fc4802cc3feda60")]
-	[InlineData(@"66e94bd4ef8a2c3b884cfa59ca342b2e", @"0388dace60b6a392f328c2b971b2fe78", @"5e2ec746917062882c85b0685353deb7")]
-	[InlineData(@"66e94bd4ef8a2c3b884cfa59ca342b2e", @"0388dace60b6a392f328c2b971b2fe7800000000000000000000000000000080", @"f38cbb1ad69223dcc3457ae5b6b0f885")]
-	[InlineData(@"66e94bd4ef8a2c3b884cfa59ca342b2e", @"0388dace60b6a392f328c2b971b2fe7ad2c55bb64f", @"c1d3b69b62c9a392687aaf55d95a1df6")]
+	[MemberData(nameof(Data), MemberType = typeof(GHashTest))]
 	public void Test(string keyHex, string plainHex, string cipherHex)
 	{
 		byte[] key = keyHex.FromHex();
 		Test_Internal(new GHashSF(key), plainHex, cipherHex);
-		Test_Internal(new GHashX86(key), plainHex, cipherHex);
 		Test_Internal(GHashUtils.Create(key), plainHex, cipherHex);
+	}
+
+	[Theory(Skip = "X86", SkipUnless = nameof(TestEnvironment.TestX86), SkipType = typeof(TestEnvironment))]
+	[MemberData(nameof(Data), MemberType = typeof(GHashTest))]
+	public void TestX86(string keyHex, string plainHex, string cipherHex)
+	{
+		byte[] key = keyHex.FromHex();
+		Test_Internal(new GHashX86(key), plainHex, cipherHex);
 	}
 }
--- test/CryptoBase.Tests/TestEnvironment.cs
+++ test/CryptoBase.Tests/TestEnvironment.cs
@@ -1,6 +1,10 @@
+using System.Runtime.Intrinsics.X86;
+
 namespace CryptoBase.Tests;
 
 public static class TestEnvironment
 {
 	public static bool TestLargeMessage => false;
+
+	public static bool TestX86 => X86Base.IsSupported;
 }
