b8c4c7e33282028204846df09116a1a14d999740
Bruce Wayne <admin@bige0.com>
2025-10-22 00:15:21 +0800

    perf: faster xor

--- src/CryptoBase/FastUtils.cs
+++ src/CryptoBase/FastUtils.cs
@@ -57,33 +57,67 @@ public static class FastUtils
 		int i = 0;
 		int left = length;
 
-		while (left >= 64)
+		if (Vector512.IsHardwareAccelerated)
 		{
-			Vector512<byte> va = CreateVector512Unsafe(stream.Slice(i));
-			Vector512<byte> vb = CreateVector512Unsafe(source.Slice(i));
-			(va ^ vb).CopyTo(destination.Slice(i));
+			while (left >= Vector512<byte>.Count)
+			{
+				ref Vector512<byte> v0 = ref Unsafe.As<byte, Vector512<byte>>(ref stream.GetRef(i));
+				ref Vector512<byte> v1 = ref Unsafe.As<byte, Vector512<byte>>(ref source.GetRef(i));
+				ref Vector512<byte> dst = ref Unsafe.As<byte, Vector512<byte>>(ref destination.GetRef(i));
 
-			i += 64;
-			left -= 64;
+				dst = v0 ^ v1;
+				i += Vector512<byte>.Count;
+				left -= Vector512<byte>.Count;
+			}
 		}
 
-		if (left >= 32)
+		if (Vector256.IsHardwareAccelerated)
 		{
-			Vector256<byte> va = CreateVector256Unsafe(stream.Slice(i));
-			Vector256<byte> vb = CreateVector256Unsafe(source.Slice(i));
-			(va ^ vb).CopyTo(destination.Slice(i));
+			while (left >= Vector256<byte>.Count)
+			{
+				ref Vector256<byte> v0 = ref Unsafe.As<byte, Vector256<byte>>(ref stream.GetRef(i));
+				ref Vector256<byte> v1 = ref Unsafe.As<byte, Vector256<byte>>(ref source.GetRef(i));
+				ref Vector256<byte> dst = ref Unsafe.As<byte, Vector256<byte>>(ref destination.GetRef(i));
 
-			i += 32;
-			left -= 32;
+				dst = v0 ^ v1;
+				i += Vector256<byte>.Count;
+				left -= Vector256<byte>.Count;
+			}
 		}
 
-		if (left >= 16)
+		if (Vector128.IsHardwareAccelerated)
 		{
-			Vector128<byte> va = CreateVector128Unsafe(stream.Slice(i));
-			Vector128<byte> vb = CreateVector128Unsafe(source.Slice(i));
-			(va ^ vb).CopyTo(destination.Slice(i));
+			while (left >= Vector128<byte>.Count)
+			{
+				ref Vector128<byte> v0 = ref Unsafe.As<byte, Vector128<byte>>(ref stream.GetRef(i));
+				ref Vector128<byte> v1 = ref Unsafe.As<byte, Vector128<byte>>(ref source.GetRef(i));
+				ref Vector128<byte> dst = ref Unsafe.As<byte, Vector128<byte>>(ref destination.GetRef(i));
 
-			i += 16;
+				dst = v0 ^ v1;
+				i += Vector128<byte>.Count;
+				left -= Vector128<byte>.Count;
+			}
+		}
+
+		while (left >= sizeof(ulong))
+		{
+			ref ulong v0 = ref Unsafe.As<byte, ulong>(ref stream.GetRef(i));
+			ref ulong v1 = ref Unsafe.As<byte, ulong>(ref source.GetRef(i));
+			ref ulong dst = ref Unsafe.As<byte, ulong>(ref destination.GetRef(i));
+
+			dst = v0 ^ v1;
+			i += sizeof(ulong);
+			left -= sizeof(ulong);
+		}
+
+		if (left >= sizeof(uint))
+		{
+			ref uint v0 = ref Unsafe.As<byte, uint>(ref stream.GetRef(i));
+			ref uint v1 = ref Unsafe.As<byte, uint>(ref source.GetRef(i));
+			ref uint dst = ref Unsafe.As<byte, uint>(ref destination.GetRef(i));
+
+			dst = v0 ^ v1;
+			i += sizeof(uint);
 		}
 
 		for (; i < length; ++i)
