51d7cbd435c4464fc10f9c6ee7540834baea098a
Bruce Wayne <admin@bige0.com>
2025-11-05 21:00:30 +0800

    refactor: ghash x86

--- src/CryptoBase/Macs/GHash/GHashUtils.cs
+++ src/CryptoBase/Macs/GHash/GHashUtils.cs
@@ -7,11 +7,71 @@ public static class GHashUtils
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static IMac Create(ReadOnlySpan<byte> key)
 	{
-		if (Sse2.IsSupported && Ssse3.IsSupported && Pclmulqdq.IsSupported)
+		if (Sse2.IsSupported && Pclmulqdq.IsSupported)
 		{
 			return new GHashX86(key);
 		}
 
 		return new GHashSF(key);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	internal static Vector128<byte> GfMultiply(in Vector128<byte> a, in Vector128<byte> b)
+	{
+		GfMultiply(a, b, out Vector128<uint> lo, out Vector128<uint> hi);
+		return Reduce(lo, hi);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	internal static void GfMultiply(in Vector128<byte> a, in Vector128<byte> b, out Vector128<uint> lo, out Vector128<uint> hi)
+	{
+		Vector128<uint> p00 = Pclmulqdq.CarrylessMultiply(a.AsUInt64(), b.AsUInt64(), 0x00).AsUInt32();
+		Vector128<uint> p10 = Pclmulqdq.CarrylessMultiply(a.AsUInt64(), b.AsUInt64(), 0x10).AsUInt32();
+		Vector128<uint> p01 = Pclmulqdq.CarrylessMultiply(a.AsUInt64(), b.AsUInt64(), 0x01).AsUInt32();
+		Vector128<uint> p11 = Pclmulqdq.CarrylessMultiply(a.AsUInt64(), b.AsUInt64(), 0x11).AsUInt32();
+
+		Vector128<uint> mid = p10 ^ p01;
+		Vector128<uint> midLo = Sse2.ShiftLeftLogical128BitLane(mid, 8);
+		Vector128<uint> midHi = Sse2.ShiftRightLogical128BitLane(mid, 8);
+
+		lo = p00 ^ midLo;
+		hi = p11 ^ midHi;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	internal static Vector128<byte> Reduce(in Vector128<uint> lo, in Vector128<uint> hi)
+	{
+		Vector128<uint> tmp3 = lo;
+		Vector128<uint> tmp6 = hi;
+
+		Vector128<uint> tmp7 = tmp3 >>> 31;
+		Vector128<uint> tmp8 = tmp6 >>> 31;
+		tmp3 <<= 1;
+		tmp6 <<= 1;
+		Vector128<uint> tmp9 = Sse2.ShiftRightLogical128BitLane(tmp7, 12);
+		tmp8 = Sse2.ShiftLeftLogical128BitLane(tmp8, 4);
+		tmp7 = Sse2.ShiftLeftLogical128BitLane(tmp7, 4);
+		tmp3 |= tmp7;
+		tmp6 |= tmp8;
+		tmp6 |= tmp9;
+
+		tmp7 = tmp3 << 31;
+		tmp8 = tmp3 << 30;
+		tmp9 = tmp3 << 25;
+		tmp7 ^= tmp8;
+		tmp7 ^= tmp9;
+		tmp8 = Sse2.ShiftRightLogical128BitLane(tmp7, 4);
+		tmp7 = Sse2.ShiftLeftLogical128BitLane(tmp7, 12);
+		tmp3 ^= tmp7;
+		Vector128<uint> tmp2 = tmp3 >>> 1;
+		Vector128<uint> tmp4 = tmp3 >>> 2;
+		Vector128<uint> tmp5 = tmp3 >>> 7;
+		tmp2 ^= tmp4;
+		tmp2 ^= tmp5;
+		tmp2 ^= tmp8;
+		tmp3 ^= tmp2;
+		tmp6 ^= tmp3;
+
+		return tmp6.AsByte();
 	}
 }
--- src/CryptoBase/Macs/GHash/GHashX86.cs
+++ src/CryptoBase/Macs/GHash/GHashX86.cs
@@ -15,6 +15,11 @@ public sealed class GHashX86 : IMac
 	private readonly Vector128<byte> _key2;
 	private readonly Vector128<byte> _key3;
 	private readonly Vector128<byte> _key4;
+	private readonly Vector128<byte> _key5;
+	private readonly Vector128<byte> _key6;
+	private readonly Vector128<byte> _key7;
+	private readonly Vector128<byte> _key8;
+
 	private Vector128<byte> _buffer;
 
 	public GHashX86(scoped ReadOnlySpan<byte> key)
@@ -23,58 +28,16 @@ public sealed class GHashX86 : IMac
 
 		ref readonly Vector128<byte> v = ref Unsafe.As<byte, Vector128<byte>>(ref key.GetReference());
 		_key1 = v.ReverseEndianness128();
-		_key2 = GfMultiply(_key1, _key1);
-		_key3 = GfMultiply(_key2, _key1);
-		_key4 = GfMultiply(_key3, _key1);
+
+		_key2 = GHashUtils.GfMultiply(_key1, _key1);
+		_key3 = GHashUtils.GfMultiply(_key2, _key1);
+		_key4 = GHashUtils.GfMultiply(_key3, _key1);
+		_key5 = GHashUtils.GfMultiply(_key4, _key1);
+		_key6 = GHashUtils.GfMultiply(_key5, _key1);
+		_key7 = GHashUtils.GfMultiply(_key6, _key1);
+		_key8 = GHashUtils.GfMultiply(_key7, _key1);
 
 		Reset();
-	}
-
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	private static Vector128<byte> GfMultiply(Vector128<byte> x, Vector128<byte> y)
-	{
-		Vector128<ulong> a = x.AsUInt64();
-		Vector128<ulong> b = y.AsUInt64();
-
-		Vector128<uint> tmp3 = Pclmulqdq.CarrylessMultiply(a, b, 0x00).AsUInt32();
-		Vector128<uint> tmp4 = Pclmulqdq.CarrylessMultiply(a, b, 0x10).AsUInt32();
-		Vector128<uint> tmp5 = Pclmulqdq.CarrylessMultiply(a, b, 0x01).AsUInt32();
-		Vector128<uint> tmp6 = Pclmulqdq.CarrylessMultiply(a, b, 0x11).AsUInt32();
-
-		tmp4 ^= tmp5;
-		tmp5 = Sse2.ShiftLeftLogical128BitLane(tmp4, 8);
-		tmp4 = Sse2.ShiftRightLogical128BitLane(tmp4, 8);
-		tmp3 ^= tmp5;
-		tmp6 ^= tmp4;
-
-		Vector128<uint> tmp7 = tmp3 >>> 31;
-		Vector128<uint> tmp8 = tmp6 >>> 31;
-		tmp3 <<= 1;
-		tmp6 <<= 1;
-		Vector128<uint> tmp9 = Sse2.ShiftRightLogical128BitLane(tmp7, 12);
-		tmp8 = Sse2.ShiftLeftLogical128BitLane(tmp8, 4);
-		tmp7 = Sse2.ShiftLeftLogical128BitLane(tmp7, 4);
-		tmp3 |= tmp7;
-		tmp6 |= tmp8;
-		tmp6 |= tmp9;
-		tmp7 = tmp3 << 31;
-		tmp8 = tmp3 << 30;
-		tmp9 = tmp3 << 25;
-		tmp7 ^= tmp8;
-		tmp7 ^= tmp9;
-		tmp8 = Sse2.ShiftRightLogical128BitLane(tmp7, 4);
-		tmp7 = Sse2.ShiftLeftLogical128BitLane(tmp7, 12);
-		tmp3 ^= tmp7;
-		Vector128<uint> tmp2 = tmp3 >>> 1;
-		tmp4 = tmp3 >>> 2;
-		tmp5 = tmp3 >>> 7;
-		tmp2 ^= tmp4;
-		tmp2 ^= tmp5;
-		tmp2 ^= tmp8;
-		tmp3 ^= tmp2;
-		tmp6 ^= tmp3;
-
-		return tmp6.AsByte();
 	}
 
 	public void Update(scoped ReadOnlySpan<byte> source)
@@ -83,18 +46,59 @@ public sealed class GHashX86 : IMac
 		int length = source.Length;
 		ref byte ptr = ref source.GetReference();
 
+		while (length >= 8 * BlockSize)
+		{
+			Vector128<byte> x0 = Unsafe.As<byte, Vector128<byte>>(ref Unsafe.Add(ref ptr, offset + 0 * BlockSize)).ReverseEndianness128();
+			Vector128<byte> x1 = Unsafe.As<byte, Vector128<byte>>(ref Unsafe.Add(ref ptr, offset + 1 * BlockSize)).ReverseEndianness128();
+			Vector128<byte> x2 = Unsafe.As<byte, Vector128<byte>>(ref Unsafe.Add(ref ptr, offset + 2 * BlockSize)).ReverseEndianness128();
+			Vector128<byte> x3 = Unsafe.As<byte, Vector128<byte>>(ref Unsafe.Add(ref ptr, offset + 3 * BlockSize)).ReverseEndianness128();
+			Vector128<byte> x4 = Unsafe.As<byte, Vector128<byte>>(ref Unsafe.Add(ref ptr, offset + 4 * BlockSize)).ReverseEndianness128();
+			Vector128<byte> x5 = Unsafe.As<byte, Vector128<byte>>(ref Unsafe.Add(ref ptr, offset + 5 * BlockSize)).ReverseEndianness128();
+			Vector128<byte> x6 = Unsafe.As<byte, Vector128<byte>>(ref Unsafe.Add(ref ptr, offset + 6 * BlockSize)).ReverseEndianness128();
+			Vector128<byte> x7 = Unsafe.As<byte, Vector128<byte>>(ref Unsafe.Add(ref ptr, offset + 7 * BlockSize)).ReverseEndianness128();
+			x0 ^= _buffer;
+
+			GHashUtils.GfMultiply(_key8, x0, out Vector128<uint> lo0, out Vector128<uint> hi0);
+			GHashUtils.GfMultiply(_key7, x1, out Vector128<uint> lo1, out Vector128<uint> hi1);
+			GHashUtils.GfMultiply(_key6, x2, out Vector128<uint> lo2, out Vector128<uint> hi2);
+			GHashUtils.GfMultiply(_key5, x3, out Vector128<uint> lo3, out Vector128<uint> hi3);
+			GHashUtils.GfMultiply(_key4, x4, out Vector128<uint> lo4, out Vector128<uint> hi4);
+			GHashUtils.GfMultiply(_key3, x5, out Vector128<uint> lo5, out Vector128<uint> hi5);
+			GHashUtils.GfMultiply(_key2, x6, out Vector128<uint> lo6, out Vector128<uint> hi6);
+			GHashUtils.GfMultiply(_key1, x7, out Vector128<uint> lo7, out Vector128<uint> hi7);
+			Vector128<byte> y0 = GHashUtils.Reduce(lo0, hi0);
+			Vector128<byte> y1 = GHashUtils.Reduce(lo1, hi1);
+			Vector128<byte> y2 = GHashUtils.Reduce(lo2, hi2);
+			Vector128<byte> y3 = GHashUtils.Reduce(lo3, hi3);
+			Vector128<byte> y4 = GHashUtils.Reduce(lo4, hi4);
+			Vector128<byte> y5 = GHashUtils.Reduce(lo5, hi5);
+			Vector128<byte> y6 = GHashUtils.Reduce(lo6, hi6);
+			Vector128<byte> y7 = GHashUtils.Reduce(lo7, hi7);
+
+			_buffer = y0 ^ y1 ^ y2 ^ y3 ^ y4 ^ y5 ^ y6 ^ y7;
+
+			offset += 8 * BlockSize;
+			length -= 8 * BlockSize;
+		}
+
 		while (length >= 4 * BlockSize)
 		{
 			Vector128<byte> x0 = Unsafe.As<byte, Vector128<byte>>(ref Unsafe.Add(ref ptr, offset + 0 * BlockSize)).ReverseEndianness128();
 			Vector128<byte> x1 = Unsafe.As<byte, Vector128<byte>>(ref Unsafe.Add(ref ptr, offset + 1 * BlockSize)).ReverseEndianness128();
 			Vector128<byte> x2 = Unsafe.As<byte, Vector128<byte>>(ref Unsafe.Add(ref ptr, offset + 2 * BlockSize)).ReverseEndianness128();
 			Vector128<byte> x3 = Unsafe.As<byte, Vector128<byte>>(ref Unsafe.Add(ref ptr, offset + 3 * BlockSize)).ReverseEndianness128();
+			x0 ^= _buffer;
 
-			x0 = GfMultiply(_key4, x0 ^ _buffer);
-			x1 = GfMultiply(_key3, x1);
-			x2 = GfMultiply(_key2, x2);
-			x3 = GfMultiply(_key1, x3);
-			_buffer = x0 ^ x1 ^ x2 ^ x3;
+			GHashUtils.GfMultiply(_key4, x0, out Vector128<uint> lo0, out Vector128<uint> hi0);
+			GHashUtils.GfMultiply(_key3, x1, out Vector128<uint> lo1, out Vector128<uint> hi1);
+			GHashUtils.GfMultiply(_key2, x2, out Vector128<uint> lo2, out Vector128<uint> hi2);
+			GHashUtils.GfMultiply(_key1, x3, out Vector128<uint> lo3, out Vector128<uint> hi3);
+			Vector128<byte> y0 = GHashUtils.Reduce(lo0, hi0);
+			Vector128<byte> y1 = GHashUtils.Reduce(lo1, hi1);
+			Vector128<byte> y2 = GHashUtils.Reduce(lo2, hi2);
+			Vector128<byte> y3 = GHashUtils.Reduce(lo3, hi3);
+
+			_buffer = y0 ^ y1 ^ y2 ^ y3;
 
 			offset += 4 * BlockSize;
 			length -= 4 * BlockSize;
@@ -102,12 +106,16 @@ public sealed class GHashX86 : IMac
 
 		if (length >= 2 * BlockSize)
 		{
-			Vector128<byte> v0 = Unsafe.As<byte, Vector128<byte>>(ref Unsafe.Add(ref ptr, offset + 0 * BlockSize)).ReverseEndianness128();
-			Vector128<byte> v1 = Unsafe.As<byte, Vector128<byte>>(ref Unsafe.Add(ref ptr, offset + 1 * BlockSize)).ReverseEndianness128();
+			Vector128<byte> x0 = Unsafe.As<byte, Vector128<byte>>(ref Unsafe.Add(ref ptr, offset + 0 * BlockSize)).ReverseEndianness128();
+			Vector128<byte> x1 = Unsafe.As<byte, Vector128<byte>>(ref Unsafe.Add(ref ptr, offset + 1 * BlockSize)).ReverseEndianness128();
+			x0 ^= _buffer;
 
-			v0 = GfMultiply(_key2, v0 ^ _buffer);
-			v1 = GfMultiply(_key1, v1);
-			_buffer = v0 ^ v1;
+			GHashUtils.GfMultiply(_key4, x0, out Vector128<uint> lo0, out Vector128<uint> hi0);
+			GHashUtils.GfMultiply(_key3, x1, out Vector128<uint> lo1, out Vector128<uint> hi1);
+			Vector128<byte> y0 = GHashUtils.Reduce(lo0, hi0);
+			Vector128<byte> y1 = GHashUtils.Reduce(lo1, hi1);
+
+			_buffer = y0 ^ y1;
 
 			offset += 2 * BlockSize;
 			length -= 2 * BlockSize;
@@ -116,7 +124,7 @@ public sealed class GHashX86 : IMac
 		if (length >= BlockSize)
 		{
 			Vector128<byte> v = Unsafe.As<byte, Vector128<byte>>(ref Unsafe.Add(ref ptr, offset)).ReverseEndianness128();
-			_buffer = GfMultiply(_key1, v ^ _buffer);
+			_buffer = GHashUtils.GfMultiply(_key1, v ^ _buffer);
 
 			offset += BlockSize;
 			length -= BlockSize;
@@ -128,7 +136,7 @@ public sealed class GHashX86 : IMac
 			source.Slice(offset).CopyTo(block);
 
 			Vector128<byte> v = Unsafe.As<byte, Vector128<byte>>(ref block.GetReference()).ReverseEndianness128();
-			_buffer = GfMultiply(_key1, v ^ _buffer);
+			_buffer = GHashUtils.GfMultiply(_key1, v ^ _buffer);
 		}
 	}
 
