981df2c8641053aa196c8dc43a87be16c9dea66e
Bruce Wayne <admin@bige0.com>
2025-11-20 10:36:04 +0800

    fix: test environment

--- test/CryptoBase.Tests/TestEnvironment.cs
+++ test/CryptoBase.Tests/TestEnvironment.cs
@@ -2,9 +2,39 @@ using System.Runtime.Intrinsics.X86;
 
 namespace CryptoBase.Tests;
 
-public static class TestEnvironment
+public class TestEnvironment
 {
 	public static bool TestLargeMessage => false;
 
 	public static bool TestX86 => X86Base.IsSupported;
+
+	/// <summary>
+	/// https://github.com/dotnet/runtime/blob/main/src/coreclr/jit/jitconfigvalues.h
+	/// </summary>
+	[Fact]
+	public void IntrinsicDisableEnvironmentTest()
+	{
+		if (X86Base.IsSupported)
+		{
+			if (Environment.GetEnvironmentVariable("DOTNET_EnableHWIntrinsic") is "0")
+			{
+				Assert.False(Sse2.IsSupported);
+				Assert.False(Avx.IsSupported);
+				Assert.False(Avx2.IsSupported);
+				Assert.False(Avx512F.IsSupported);
+			}
+
+			if (Environment.GetEnvironmentVariable("DOTNET_EnableAVX") is "0")
+			{
+				Assert.False(Avx.IsSupported);
+				Assert.False(Avx2.IsSupported);
+				Assert.False(Avx512F.IsSupported);
+			}
+
+			if (Environment.GetEnvironmentVariable("DOTNET_EnableAVX512") is "0")
+			{
+				Assert.False(Avx512F.IsSupported);
+			}
+		}
+	}
 }
